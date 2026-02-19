60aaec03cf66eb0a7d82f5fa475b8bfc04fec8d5
Bonna05 <bonnagoala@gmail.com>
2026-02-20 03:26:37 +0600

    find max using function

---
+++ max.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+int max_of_four(int a,int b,int c,int d){
+
+int max;
+if(a>b && a>c && a>d){
+                max=a;
+}
+else if(b>a && b>c && b>d){
+                max=b;
+}
+else if(c>a && c>b && c>d){
+                max=c;
+}
+else {
+                max=d;
+}
+return max;
+}
+int main()
+{
+                int a,b,c,d;
+                cin>>a>>b>>c>>d;
+                int result=max_of_four(a,b,c,d);
+                cout<<result;
+                return 0;
+}
